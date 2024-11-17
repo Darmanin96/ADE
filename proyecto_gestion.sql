43da0b1adf67dfca21aca2572ee170b77640050d
Darmanin96 <165905936+Darmanin96@users.noreply.github.com>
2024-11-17 23:56:34 +0000

    Update proyecto_gestion.sql

--- proyecto_gestion.sql
+++ proyecto_gestion.sql
@@ -7,6 +7,10 @@
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.2
 
+
+CREATE DATABASE IF NOT EXISTS proyecto_gestion;
+USE proyecto_gestion
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
